14ac97da4444618832bf6a7816a2128a5a6228f2
dieprr <sandsan755@gmail.com>
2024-10-09 15:49:49 -0600

    Running animation + turning
    Added running animation as well as fixed small bridge collision bug.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,11 +13,20 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the player sprite based on movement direction
+	if direction < 0:
+		$AnimatedSprite2D.scale.x = -1  # Face left
+	elif direction > 0:
+		$AnimatedSprite2D.scale.x = 1   # Face right
+
+	# Set velocity based on movement direction
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("Run")  # Play run animation when moving
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("Idle")  # Play idle animation when not moving
 
 	move_and_slide()
