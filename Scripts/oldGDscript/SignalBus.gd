82d09c69d72bd5f2dc7108a6e904e2248cda1bd0
Viet Ngomai <thienvietngomai@gmail.com>
2024-11-12 03:26:41 -0500

    Deleted duplicate files(?)

--- Scripts/oldGDscript/SignalBus.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# define global signals needed here
-
-signal enemyEnteredAttackRange(enemy: Node2D)
-signal enemyLeftAttackRange(enemy: Node2D)
-
-# Combat UI signals
-signal showEnemyInRangeUI(show: bool)
