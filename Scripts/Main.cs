57c6b6cf841af98170eab1a1207feea28594607e
Viet Ngomai <thienvietngomai@gmail.com>
2024-12-20 00:55:29 -0500

    Add ESC as option to quit
    Added ESC keybind to easily quit the game, also added print statement to check if running in debug or release build

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 // Added to global scripts
 public partial class Main : Node
@@ -7,10 +8,35 @@ public partial class Main : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        IsDebug();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        CheckForQuit();
     }
+
+    private void CheckForQuit()
+    {
+        if (Input.IsActionJustPressed("Quit"))
+        {
+            GetTree().Quit(); // Quit the game
+            GD.Print("Escape pressed, quitting the game");
+        }
+    }
+
+    private void IsDebug()
+    {
+        if (OS.IsDebugBuild())
+        {
+            GD.Print("Running in debug build");
+        }
+        else
+        {
+            GD.Print("Running in release build");
+        }
+
+    }
+
 }
